8b127e637bda149b56a46447629db0cb4c9f882c
SARATHCHANDRA <sarathchandra76@gmail.com>
2021-11-11 15:35:33 +0530

    new changes

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SealedclassSample.DSealedclassSample
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello!");
+        }
+   } 
+    
+}
